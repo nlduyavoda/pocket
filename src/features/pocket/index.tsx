83767b83ef6f0f09c836024c3c6722bdc9c08bda
nlduy <nguyenleduy1199@gmail.com>
2024-01-10 17:53:35 +0700

    initial form

--- src/features/pocket/index.tsx
+++ src/features/pocket/index.tsx
@@ -1,3 +1,105 @@
+"use client";
+import { defaultMonthlyExpenses, MonthlyExpenses } from "@utils/variables";
+import { Fragment } from "react";
+import { Controller, FormProvider, useForm } from "react-hook-form";
+import {
+  Button,
+  Checkbox,
+  ConfigProvider,
+  Form,
+  Input,
+  Typography,
+} from "antd";
+
 export const PocketForm = () => {
-  return <div>this is pocket form</div>;
+  const methods = useForm<MonthlyExpenses>({
+    defaultValues: defaultMonthlyExpenses,
+  });
+
+  const onSubmit = (params: MonthlyExpenses) => {};
+
+  console.log(methods.getValues());
+  const formValues: MonthlyExpenses = methods.getValues();
+  const formKeys = Object.keys(formValues) as (keyof MonthlyExpenses)[];
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          Typography: {
+            colorPrimary: "#fff",
+          },
+          Button: {
+            colorPrimary: "#00b96b",
+            algorithm: true, // Enable algorithm
+          },
+          Input: {
+            colorPrimary: "#eb2f96",
+            algorithm: true, // Enable algorithm
+          },
+        },
+      }}
+    >
+      <FormProvider {...methods}>
+        <Form
+          name="basic"
+          className="p-10"
+          layout="vertical"
+          initialValues={{ remember: true }}
+          onFinish={methods.handleSubmit(onSubmit)}
+          autoComplete="off"
+          style={{
+            maxWidth: 600,
+            alignContent: "center",
+            display: "flex",
+            flexDirection: "column",
+          }}
+        >
+          {formKeys.map((ele: keyof MonthlyExpenses, idx) => {
+            return (
+              <Form.Item key={idx}>
+                <Typography.Title className="text-white" level={3}>
+                  {ele}
+                </Typography.Title>
+                <Controller
+                  control={methods.control}
+                  name={ele}
+                  render={({ field }) => {
+                    const fieldValues = field.value as Partial<MonthlyExpenses>;
+                    const fieldKeys = Object.keys(
+                      fieldValues
+                    ) as (keyof MonthlyExpenses)[];
+                    return (
+                      <>
+                        {fieldKeys.map((subKey: keyof MonthlyExpenses, idx) => {
+                          const subValues = fieldValues[subKey];
+                          console.log(
+                            "subValues?.toString()",
+                            subValues?.toString()
+                          );
+                          return (
+                            <Fragment key={subKey}>
+                              <Typography.Title
+                                className="text-white"
+                                level={5}
+                              >
+                                {subKey}
+                              </Typography.Title>
+                              <Input
+                                placeholder={subKey}
+                                value={subValues?.toString()}
+                              />
+                            </Fragment>
+                          );
+                        })}
+                      </>
+                    );
+                  }}
+                />
+              </Form.Item>
+            );
+          })}
+        </Form>
+      </FormProvider>
+    </ConfigProvider>
+  );
 };
--- src/utils/variables.ts
+++ src/utils/variables.ts
@@ -1,3 +1,92 @@
 export const sum = (a: number, b: number) => {
   return a + b;
 };
+
+export type MonthlyExpenses = {
+  housing: {
+    rent_or_mortgage: number;
+    utilities: number;
+    home_maintenance: number;
+  };
+  transportation: {
+    car_payment: number;
+    insurance: number;
+    fuel: number;
+    public_transport: number;
+  };
+  groceries: number;
+  healthcare: {
+    insurance: number;
+    prescriptions: number;
+    other_medical_expenses: number;
+  };
+  entertainment: {
+    dining_out: number;
+    subscriptions: number;
+    movies_and_events: number;
+  };
+  personal_care: {
+    haircuts: number;
+    toiletries: number;
+  };
+  debts: {
+    credit_card_payments: number;
+    loan_payments: number;
+  };
+  savings: {
+    emergency_fund: number;
+    retirement: number;
+    other_savings: number;
+  };
+  other_expenses: {
+    gifts_and_donations: number;
+    miscellaneous: number;
+  };
+  total_expenses: number;
+};
+
+// Example usage
+export const defaultMonthlyExpenses: MonthlyExpenses = {
+  housing: {
+    rent_or_mortgage: 1200,
+    utilities: 150,
+    home_maintenance: 50,
+  },
+  transportation: {
+    car_payment: 300,
+    insurance: 100,
+    fuel: 80,
+    public_transport: 40,
+  },
+  groceries: 300,
+  healthcare: {
+    insurance: 200,
+    prescriptions: 50,
+    other_medical_expenses: 20,
+  },
+  entertainment: {
+    dining_out: 100,
+    subscriptions: 20,
+    movies_and_events: 30,
+  },
+  personal_care: {
+    haircuts: 20,
+    toiletries: 30,
+  },
+  debts: {
+    credit_card_payments: 100,
+    loan_payments: 50,
+  },
+  savings: {
+    emergency_fund: 200,
+    retirement: 150,
+    other_savings: 50,
+  },
+  other_expenses: {
+    gifts_and_donations: 30,
+    miscellaneous: 40,
+  },
+  total_expenses: 2770, // Sum of all expenses
+};
+
+
